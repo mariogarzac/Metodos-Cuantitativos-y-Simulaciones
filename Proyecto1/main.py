1f37d2aa4eb97e76ec8478384d93c8fc6ac8ebe7
Mario Garza Chapa <mariogarza@mariogarzac.lan>
2022-07-03 19:28:55 -0500

    Initial Commit

---
+++ Proyecto1/main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+# Mario Garza Chapa
+# Juan Pablo González
+# Michel Antoine Dionne
+
+import chi 
+import rachas
+
+def main():
+
+    chi.chiSquaredTest()
+    rachas.runsTest()
+
+if __name__ == "__main__":
+    main()
